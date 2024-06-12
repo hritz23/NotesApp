b4b63a10a4a4d9be2b89c3ee4ed2569676a35e24
HritikaNaik <hritikan232@gmail.com>
2024-06-12 17:45:52 +0530

    initialization and created viewModel

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -5,10 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.Adapter.NotesAdapter
+import com.example.notesapp.Database.NoteDatabase
+import com.example.notesapp.Models.NoteViewModel
+import com.example.notesapp.Models.Notes
+import com.example.notesapp.databinding.ActivityMainBinding
+import org.w3c.dom.Node
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database : NoteDatabase
+    lateinit var viewModel: NoteViewModel
+    lateinit var adapter: NotesAdapter
+    lateinit var selectNode : Notes
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+
+        initUi()
+
+    }
+
+    private fun initUi() {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/notesapp/Models/NoteViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.notesapp.Models
+
+class NoteViewModel {
+}
